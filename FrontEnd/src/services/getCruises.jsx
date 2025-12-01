fcb7e310289305196a8e98ed4a9e91e3e19a0efd
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2025-12-01 13:59:46 -0300

    update getCruises

--- FrontEnd/src/services/getCruises.jsx
+++ FrontEnd/src/services/getCruises.jsx
@@ -1,18 +1,7 @@
+
 export async function getCruises() {
-  const username = "Gabriel Marques";
-  const appPassword = "Yerm ri9e Bo4J xpyB TD5g 548E";
-
-  const token = btoa(`${username}:${appPassword}`.trim());
-
-  const result = await fetch(
-    "https://manualdoagente.com.br/wp-json/custom/v1/ofertas-azamara",
-    {
-      headers: {
-        Authorization: `Basic ${token}`,
-      },
-    }
-  );
-
-  const data = await result.json();
-  return data;
+    const result = await fetch('https://manualdoagente.com.br/wp-json/custom/v1/ofertas-azamara');
+    const data = await result.json();
+    return data
 }
+
